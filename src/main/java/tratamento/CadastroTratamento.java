25298dd95d0fa3c554c31f41bc0e462a74ae739d
Davi Esteves <desenvolvedores@codejr.com.br>
2021-07-10 17:01:36 -0300

    todos arquivos com matriculas e nomes

--- src/main/java/tratamento/CadastroTratamento.java
+++ src/main/java/tratamento/CadastroTratamento.java
@@ -1,5 +1,8 @@
-package tratamento;
+//Anna Letícia Franco Monteiro - 202065106A
+//Davi Esteves dos Santos - 202065504B
+//Pedro Campos Lima - 202065521B
 
+package tratamento;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
