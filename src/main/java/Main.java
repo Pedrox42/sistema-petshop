2b50982029d20959f26463c83ec1d4004453803a
Davi Esteves <desenvolvedores@codejr.com.br>
2021-08-27 17:42:01 -0300

    correcao

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ import view.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        Arquivo.preencherTodos();
         TelaLogin tela = new TelaLogin();
         tela.setVisible(true);
     }
